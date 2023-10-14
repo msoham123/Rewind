14b883ac00c8bf8806e2339a7dcf954d51ddc795
Sammy Taubman <staubman1@gmail.com>
2023-10-14 17:35:03 -0400

    Firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:Rewind/services/FirestoreService.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:Rewind/models/Message.dart';
+import 'package:Rewind/auth/SignIn.dart';
+import 'package:Rewind/auth/home_screen.dart';
+import 'package:page_transition/page_transition.dart';
 import 'firebase_options.dart';
+
+import 'package:Rewind/main.dart';
 
 void main() async {
   await Firebase.initializeApp(
@@ -39,115 +44,19 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      // sign in page, onsignin navigate to MyHomePage
+      home: SignIn(
+        onSignIn: () {
+          print("HI");
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MyHomePage(title: "HI")),
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  FirestoreService _db = FirestoreService();
-
-  void _listMessages() async {
-    List<Message> messages = await _db.getMessages();
-    messages.forEach((msg) {
-      print(msg.toJson());
-    });
-  }
-
-  void _addTestMessage() async {
-    Message msg = Message(
-      author: 'w4ciwehu54',
-      content: 'Hello world',
-      emoji: '🔥',
-      timestamp: Timestamp.now(),
-      lat: 10.4,
-      long: 15.6
-    );
-    await _db.pushMessage(msg);
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+            );
+        },
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _listMessages,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
