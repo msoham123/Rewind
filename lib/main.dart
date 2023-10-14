058c9640c1bfdd5525b81ef30ab36fd217958362
Sammy Taubman <staubman1@gmail.com>
2023-10-14 18:06:23 -0400

    Firebase fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:Rewind/ar_screen.dart';
 import 'package:Rewind/location_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
