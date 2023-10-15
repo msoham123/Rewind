c31bf2132b1b85693eed3c945b9a39e6e993c8eb
Sammy Taubman <staubman1@gmail.com>
2023-10-15 01:17:41 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:Rewind/location_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import "firebase_options.dart";
 import 'homepage.dart';
 import 'firebase_options.dart';
 
